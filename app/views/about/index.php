f50e4b6f55a09634a073b828fd4e0971a397ccf6
ilhamriz <milhamrizky7@gmail.com>
2025-07-16 18:26:38 +0700

    remove unused

--- app/views/about/index.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-  <h1 class="mt-4">About Me</h1>
-  <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Profile" width="200" class="rounded-circle">
-  <p>Halo, nama saya <?= $data["nama"] ?>. Saya seorang <?= $data["pekerjaan"] ?></p>
-</div>
--- app/views/home/index.php
+++
@@ -1,4 +0,0 @@
-<div class="container">
-  <h1 class="mt-4">Hello</h1>
-  <p>Saya <?= $data['nama']; ?></p>
-</div>
